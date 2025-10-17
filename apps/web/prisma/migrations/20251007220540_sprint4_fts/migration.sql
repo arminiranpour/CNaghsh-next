fd8447b0f80108910d139b71dc776acb1697926f
arminiranpour <iranpour.armin.ca@gmail.com>
2025-10-17 19:34:41 -0400

    .

--- apps/web/prisma/migrations/20251007220540_sprint4_fts/migration.sql
+++ apps/web/prisma/migrations/20251007220540_sprint4_fts/migration.sql
@@ -5,14 +5,13 @@
   - You are about to drop the column `search_vector` on the `Profile` table. All the data in the column will be lost.
 
 */
--- DropIndex
-DROP INDEX "public"."idx_job_search_vector";
+DROP INDEX IF EXISTS "public"."idx_job_search_vector";
 
 -- DropIndex
-DROP INDEX "public"."idx_profile_search_vector";
+DROP INDEX IF EXISTS "public"."idx_profile_search_vector";
 
 -- AlterTable
-ALTER TABLE "Job" DROP COLUMN "search_vector";
+ALTER TABLE "Job" DROP COLUMN IF EXISTS "search_vector";
 
 -- AlterTable
-ALTER TABLE "Profile" DROP COLUMN "search_vector";
+ALTER TABLE "Profile" DROP COLUMN IF EXISTS "search_vector";
